edff86342a3290d8a91fc75bb4bdea6639fd8a48
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 13:18:40 +0300

    import connect from react-redux and map state to props. List all jobs

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import JobTile from './JobTile'
 import styles from './JobList.module.css'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 
-const JobList = () => {
+
+const JobList = ({ jobs }) => {
   return (
     <div className={styles.card_container}>
-      <JobTile />
-      <JobTile />
-      <JobTile />
-      <JobTile />
-      <JobTile />
-      <JobTile />
-      <JobTile />
+      {jobs && jobs.map(job => <Link to={'/jobs/' + job.id} key={job.id}> <JobTile job={job} /></Link>)}
     </div>
   )
 }
 
-export default JobList
+const mapStateToProps = (state) => {
+  return {
+    jobs: state.jobs
+  }
+}
+
+
+export default connect(mapStateToProps)(JobList)
