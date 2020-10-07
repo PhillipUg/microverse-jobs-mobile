6527ed3e348ee20a6012b57db06e5ead5593b68b
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 22:53:42 +0300

    onComponent mount, fetch data and handle filter on change

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -3,22 +3,59 @@ import JobTile from './JobTile'
 import styles from './JobList.module.css'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Filter from './Filter'
+import { changeFilter, fetchJobs } from '../actions/index'
 
 
 
-const JobList = ({ jobs }) => {
-  return (
-    <div className={styles.card_container}>
-      {jobs && jobs.map(job => <Link to={'/jobs/' + job.id} key={job.id}> <JobTile job={job} /></Link>)}
-    </div>
-  )
+
+class JobList extends React.Component {
+  componentDidMount() {
+    this.props.fetchJobs()
+  }
+
+  handleFilter = (e) => {
+    this.props.changeFilter(e.target.value)
+  }
+
+  render() {
+    const { jobs, filter } = this.props
+    let jobList;
+    if (filter === 'All') {
+      jobList = jobs.map(job => (
+        <Link to={'/jobs/' + job.id} key={job.id}><JobTile job={job} key={job.id} /></Link>
+      ));
+    } else {
+      jobList = jobs.filter(job => job.type === filter).map(job => (
+        <Link to={'/jobs/' + job.id} key={job.id}><JobTile job={job} key={job.id} /></Link>
+      ));
+    }
+    return (
+      <div>
+        <Filter handleFilter={this.handleFilter} />
+        <div className={styles.card_container}>
+          {jobList}
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
+  console.log(state)
   return {
-    jobs: state.jobs
+    jobs: state.jobsReducer.jobs,
+    filter: state.filterReducer
+  }
+}
+
+const mapDispatchToFilter = (dispatch) => {
+  return {
+    changeFilter: (filter) => { dispatch(changeFilter(filter)) },
+    fetchJobs: () => dispatch(fetchJobs())
   }
 }
 
 
-export default connect(mapStateToProps)(JobList)
+
+export default connect(mapStateToProps, mapDispatchToFilter)(JobList)
