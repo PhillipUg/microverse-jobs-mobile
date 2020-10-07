5e5747e79adfe51465997d76f865f1dabb70f3e3
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 22:44:00 +0300

    import thunk and applyMiddleware and pass them into redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
