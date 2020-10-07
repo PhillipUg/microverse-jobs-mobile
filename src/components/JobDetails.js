32cd8c86a7baefdea459730cdf912d38e0156730
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 22:52:44 +0300

    Make jobdetails recieve display data dynamically

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,21 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const JobDetails = () => {
+const JobDetails = ({ job }) => {
   return (
     <div>
-       <p>"id": "d98ef1c8-0559-4784-a18c-12f0b21d0315",</p >
-        <p>Type: "Full time"</p>
-      <p>URL: "https://jobs.github.com/positions/d98ef1c8-0559-4784-a18c-12f0b21d0315"</p>
-      <p>Date Posted: "Mon Oct 05 16:37:44 UTC 2020"</p>
-      <p> "company": "Agrian Inc."</p>
-      <p> "company_url": "http://www.agrian.com",</p>
-       <p> "location": "Fresno, CA",</p>
-        <p>"title": "Software Engineer - Web UI",</p>
-        "description": "<p>Position:  Software Engineer - Web UI\nLocation: U.S.\nDepartment: Engineering</p>\n<p>ABOUT THE POSITION:\nAgrian is looking for an experienced front-end developer to help us build out our next generation of products. We’re writing an ambitious front end that’s built around modern Ember and is written in TypeScript. We pride ourselves on being up to date in best practices in coding, testing, and deployment strategies.</p>\n<p>You’ll work directly with Product Management, User Experience, Engineering, and QA in a company that takes pride in quickly delivering groundbreaking solutions to its clients. You can find out more about our technology stack at this talk given at EmberConf 2020.</p>\n<p>The position is ideally based in our Clovis, CA headquarters, but the role will be remote for the foreseeable future.</p>\n<p>Virtual EmberConf 2020: Taming the Beast: Managing a really ambitious codebase by Luke Deniston</p>\n<p>REQUIREMENTS:</p>\n<p>Extensive JavaScript experience with a focus on building large, complex single-page applications\nProficient in at least one modern JavaScript framework such as Ember (preferred), Svelte, Angular, or Vue\nAble to debug and optimize cross-browser and platform web applications (for desktop and mobile)\nExperience with Node-based CLI tools, JS build tools, and automated testing frameworks (QUnit, Mocha, Jasmine, etc.)\nComfortable with Git/version control workflows</p>\n<p>NICE TO HAVES:</p>\n<p>Ability to bring custom and responsive designs to life using technologies like PostCSS, Sass or Less, and frameworks like Tailwind or Bootstrap.\nKnowledge and passion for design and UX principles of web and mobile interfaces\nExperience with TypeScript (preferred) or Flow\nExperience with OpenLayers, Leaflet or other web mapping tools\nExperience with D3, Vega, or other visualization libraries\nExperience with Ruby on Rails</p>\n<p>When filling out your application, be sure to include a short letter as to why you're interested in the role and please include a link to a git repo so we can review some code samples.</p>\n<p>PHYSICAL DEMANDS:</p>\n<p>The physical demands described here are representative of those that must be met by an employee to successfully perform the essential functions of this job.  Reasonable accommodations may be made to enable individuals with disabilities to perform the essential functions.</p>\n<p>While performing the duties of this job, the employee may be required to climb or balance, and is regularly required to talk or hear or smell.</p>\n<p>The employee is occasionally required to stand; walk; sit; use hands to finger, handle, or feel objects, tools, or controls; reach with hands and arms; and stoop, kneel, or crouch.</p>\n<p>The employee must occasionally lift and/or move up to 10 pounds.</p>\n<p>Specific vision abilities required by this job include close vision, distance vision, color vision, peripheral vision, depth perception, and the ability to adjust focus.</p>\n<p>WORK ENVIRONMENT:</p>\n<p>The work environment characteristics described here are representative of those an employee encounters while performing the essential functions of this job. Reasonable accommodations may be made to enable individuals with disabilities to perform the essential functions.\nTelecommuting\nTravel can be approximately 25-50%\nThe noise level in the work environment is usually moderate.</p>\n<p>Employment Type: Full Time\nFunctional Area: Non-Management</p>\n<p>A Little About Us</p>\n<p>Founded in 2004, Agrian Inc. offers the most integrated solution to the growing demand for information addressing food safety, environmental concerns, traceability, sustainability, and locally and organically grown food. Agrian’s mission is to make technology work better for those working in Agriculture. We are turning the Agriculture marketplace upside down by providing SaaS products that deliver high value and improve efficiencies. We have well over 60,000+ customers.</p>\n<p>Agrian is a Fresno-based company with over 120 employees and offices in La Salle, Manitoba, Canada; Yerevan, Armenia; and Kathmandu, Nepal. Our mobile app has received national recognition and our employee retention is exceptional. More importantly, we provide challenging work, opportunities to learn, high-quality teammates, a standard-setting product, and a company on the move.</p>\n",
-        "how_to_apply": "<p><a href="https://home.agrian.com/company/careers/\">https://home.agrian.com/company/careers/</a></p>\n",
-        "company_logo": null
+      <img src={job[0].company_logo ? job[0].company_logo : "https://via.placeholder.com/700x500"} alt="company-logo" width="700"/>
+      <p>{job[0].type}</p>
+      <p>{job[0].url}</p>
+      <p>{job[0].created_at}</p>
+      <p>{job[0].company}</p>
+      <p>{job[0].company_url}</p>
+      <p>{job[0].location}</p>
+      <p>{job[0].title}</p>
+      <p>{job[0].description}</p>
     </div>
   )
 }
 
-export default JobDetails
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id
+  return {
+    job: state.jobsReducer.jobs.filter(job => job.id === id)
+  }
+}
+
+
+export default connect(mapStateToProps)(JobDetails)
