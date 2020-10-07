b7f91edc94a03a24a221d3f45d67e9734e345b53
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 12:57:59 +0300

    import redux, create store and pass in rootReducer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducer'
+import { Provider } from 'react-redux'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
