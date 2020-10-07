062429871aed35396d7b15e4e8e71dbb86c3448f
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 13:02:11 +0300

    import brwoserrouter, switch and route from react router dom and nest component routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchBar from './components/SearchBar'
 import JobList from './components/JobList'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import JobDetails from './components/JobDetails';
+
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
-      <JobList />
+      <BrowserRouter>
+        <SearchBar />
+        <Switch>
+          <Route exact path="/" component={JobList} />
+          <Route path="/jobs/:id" component={JobDetails} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
