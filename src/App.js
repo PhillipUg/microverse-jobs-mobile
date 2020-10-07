3d3a7e2c00783b3c0fc893e0b7c23f243f080228
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-07 22:47:03 +0300

    move filter component to jobsList

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import SearchBar from './components/SearchBar'
 import JobList from './components/JobList'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import JobDetails from './components/JobDetails';
 
 
 function App() {
+  
   return (
-    <div className="App">
-      <BrowserRouter>
-        <SearchBar />
+    <BrowserRouter>
+      <div className="App">
         <Switch>
           <Route exact path="/" component={JobList} />
           <Route path="/jobs/:id" component={JobDetails} />
         </Switch>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
