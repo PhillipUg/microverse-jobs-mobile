dd64bd2522fc69b0b294fec9d3bd5cf70e1881ad
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-08 14:39:38 +0300

    add proptypes validation on JobTile component and add more dynamic content

--- src/components/JobTile.js
+++ src/components/JobTile.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import styles from './JobTile.module.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styles from './JobTile.module.css';
+import jobImg from './job.png';
 
-const JobTile = ({ job }) => {
-  return (
-    <div className={styles.card}>
-      <div>
-        <img src={job.company_logo ? job.company_logo : "https://via.placeholder.com/100"} alt="company-logo" className={styles.company_logo} />
-      </div>
-      <div>{job.title}</div>
-      <div>{job.type}</div>
-      <div>
-        <span>tags</span>
-        <span>tags</span>
-        <span>tags</span>
-      </div>
-      <div>
-        <button>Apply</button>
-        <button>Save</button>
-      </div>
+/*eslint-disable*/
+const JobTile = ({ job }) => (
+  <div className={styles.card}>
+    <div>
+      <img src={job.company_logo ? job.company_logo : jobImg} alt="company-logo" className={styles.company_logo} />
     </div>
-  )
-}
+    <div className={styles.title}>{`${job.title.slice(0, 23)}...`}</div>
+    <div>{job.type}</div>
+    <div>{job.created_at.slice(0, 10)}</div>
+    <div>{job.location}</div>
+    <div className={styles.btn_container}>
+      <Link to={`/jobs/${job.id}`}>
+        <button type="button" className={[styles.btn, styles.btn__apply].join(' ')}>Apply</button>
+      </Link>
+      <button type="button" className={[styles.btn, styles.btn__save].join(' ')}>Save</button>
+    </div>
+  </div>
+);
+/*eslint-disable*/
+JobTile.protoTypes = {
+  job: PropTypes.any,
+};
 
-
-export default JobTile
+export default JobTile;
