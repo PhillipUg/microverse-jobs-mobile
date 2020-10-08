f85591c51d0cdfd7e70f81c73abfd0595bd7ba10
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-08 14:41:46 +0300

    add 3 more state properties and actions. fetch-begin, fetch-success and fetch-error

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -1,19 +1,31 @@
-
 const initState = {
   jobs: [],
-}
+  loading: false,
+  error: null,
+};
 
 const jobsReducer = (state = initState, action) => {
   switch (action.type) {
-    case "FETCH_JOBS":
+    case 'FETCH_JOBS_SUCCESS':
       return {
         ...state,
         loading: false,
-        jobs: action.data
-      }
+        jobs: action.data,
+      };
+    case 'FETCH_JOBS_BEGIN':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'FETCH_JOBS_ERROR':
+      return {
+        ...state,
+        loading: false,
+        error: action.err,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default jobsReducer
+export default jobsReducer;
