1d892a298869d06fc78c46067a23957478b041e1
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-12 23:51:44 +0300

    import jobContext and useContext hook, filter and render job

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,53 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { JobContext } from '../contexts/JobContext';
 import styles from './JobDetails.module.css';
-import jobImg from './job.png';
 
-const JobDetails = ({ job }) => (
-  <div className={styles.details}>
-    <div className={styles.img}>
-      <img src={job[0].company_logo ? job[0].company_logo : jobImg} alt="company-logo" width="300" />
-    </div>
-    <div>
-      <span>Job Title:</span>
-      {job[0].title}
-    </div>
-    <div>
-      <span>Job Type:</span>
-      {job[0].type}
-    </div>
-    <div>
-      <span>Posted on:</span>
-      {job[0].created_at}
-    </div>
-    <div>
-      <span>Location:</span>
-      {job[0].location}
-    </div>
-    <div>
-      <span>Company Name:</span>
-      {job[0].company}
-    </div>
-    <div>
-      <span>Company Url:</span>
-      <a href={job[0].company_url}>{job[0].company_url}</a>
-    </div>
-    <div className={styles.desc} dangerouslySetInnerHTML={{ __html: job[0].description }} /> {/*eslint-disable-line*/}
+const JobDetails = (props) => {
+  const { jobs } = useContext(JobContext)
+  const id = parseInt(props.match.params.id)
+  const job = jobs.filter(job => job.id === id)
 
-    <div dangerouslySetInnerHTML={{ __html: job[0].how_to_apply }} /> {/*eslint-disable-line*/}
-  </div>
-);
+  return (
 
-const mapStateToProps = (state, ownProps) => {
-  const { id } = ownProps.match.params;
-  return {
-    job: state.jobsReducer.jobs.filter(job => job.id === id),
-  };
+    <div className={styles.details}>
+
+      <div>
+        <span>Company:</span>
+        {job[0].company}
+      </div>
+      <div>
+        <span>Position:</span>
+        {job[0].position}
+      </div>
+      <div>
+        <span>Description:</span>
+        {job[0].description}
+      </div>
+
+    </div>
+  );
 };
 
-JobDetails.propTypes = {
-  job: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
-
-export default connect(mapStateToProps)(JobDetails);
+export default JobDetails;
