0a8c7efdcd2a3998d11b2e4053a7af1c568880fd
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-12 23:49:54 +0300

    import JobContextProvider and use it to wrap JobList and JobDetails components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import JobList from './components/JobList';
 import JobDetails from './components/JobDetails';
+import JobContextProvider from './contexts/JobContext';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={JobList} />
-          <Route path="/jobs/:id" component={JobDetails} />
-        </Switch>
+        <JobContextProvider>
+          <Switch>
+            <Route exact path="/" component={JobList} />
+            <Route path="/jobs/:id" component={JobDetails} />
+          </Switch>
+        </JobContextProvider>
       </div>
     </BrowserRouter>
   );
