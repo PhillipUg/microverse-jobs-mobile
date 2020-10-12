0968134e262fa81e231714b9ac723937ffd43a33
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-12 15:02:28 +0300

    add basic joblist functional component using react hooks

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,87 +1,113 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import JobTile from './JobTile';
-import styles from './JobList.module.css';
-import Filter from './Filter';
-import { changeFilter, fetchJobs } from '../actions/index';
+import React, { useState, useEffect } from 'react';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import JobTile from './JobTile';
+// import styles from './JobList.module.css';
+// import Filter from './Filter';
+// import { changeFilter, fetchJobs } from '../actions/index';
 
-class JobList extends React.Component {
-  componentDidMount() {
-    const { fetchJobs } = this.props;
-    fetchJobs();
-  }
+// class JobList extends React.Component {
+//   componentDidMount() {
+//     const { fetchJobs } = this.props;
+//     fetchJobs();
+//   }
 
-  handleFilter = e => {
-    const { changeFilter } = this.props;
-    changeFilter(e.target.value);
-  }
+//   handleFilter = e => {
+//     const { changeFilter } = this.props;
+//     changeFilter(e.target.value);
+//   }
 
-  render() {
-    const {
-      jobs, filter, error, loading,
-    } = this.props;
+//   render() {
+//     const {
+//       jobs, filter, error, loading,
+//     } = this.props;
 
-    if (error) {
-      return (
-        <div className={styles.message}>
-          Error!
-          {error.message}
+//     if (error) {
+//       return (
+//         <div className={styles.message}>
+//           Error!
+//           {error.message}
+//         </div>
+//       );
+//     }
+
+//     if (loading) {
+//       return <div className={styles.message}>Loading...</div>;
+//     }
+
+//     let jobList;
+//     if (filter === 'All') {
+//       jobList = jobs.map(job => (
+//         <JobTile job={job} key={job.id} />
+//       ));
+//     } else {
+//       jobList = jobs.filter(job => job.type === filter).map(job => (
+//         <JobTile job={job} key={job.id} />
+//       ));
+//     }
+//     return (
+//       <div>
+//         <div className={styles.filter}>
+//           <h1>Microverse Jobs</h1>
+//           <Filter handleFilter={this.handleFilter} />
+//         </div>
+//         <div className={styles.card_container}>
+//           {jobList}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => ({
+//   jobs: state.jobsReducer.jobs,
+//   filter: state.filterReducer,
+//   error: state.jobsReducer.error,
+//   loading: state.jobsReducer.loading,
+// });
+
+// const mapDispatchToFilter = dispatch => ({
+//   changeFilter: filter => { dispatch(changeFilter(filter)); },
+//   fetchJobs: () => dispatch(fetchJobs()),
+// });
+
+// JobList.propTypes = {
+//   jobs: PropTypes.arrayOf(PropTypes.object).isRequired,
+//   filter: PropTypes.string.isRequired,
+//   fetchJobs: PropTypes.func.isRequired,
+//   changeFilter: PropTypes.func.isRequired,
+//   error: PropTypes.string,
+//   loading: PropTypes.bool.isRequired,
+// };
+
+// JobList.defaultProps = {
+//   error: null,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToFilter)(JobList);
+
+
+const JobList = () => {
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    fetch('https://microverse-jobs-api.herokuapp.com/api/v1/jobs')
+      .then((res) => res.json())
+      .then(data => setJobs(data))
+      .catch((err) => console.log(err))
+  }, [jobs])
+
+
+  return (
+    <div style={{ color: 'white' }}>
+      {jobs.map((job, index) => (
+        <div key={index}>
+          {job.company} | {job.position} | {job.description}
         </div>
-      );
-    }
+      ))}
+    </div>
+  )
 
-    if (loading) {
-      return <div className={styles.message}>Loading...</div>;
-    }
-
-    let jobList;
-    if (filter === 'All') {
-      jobList = jobs.map(job => (
-        <JobTile job={job} key={job.id} />
-      ));
-    } else {
-      jobList = jobs.filter(job => job.type === filter).map(job => (
-        <JobTile job={job} key={job.id} />
-      ));
-    }
-    return (
-      <div>
-        <div className={styles.filter}>
-          <h1>Microverse Jobs</h1>
-          <Filter handleFilter={this.handleFilter} />
-        </div>
-        <div className={styles.card_container}>
-          {jobList}
-        </div>
-      </div>
-    );
-  }
 }
 
-const mapStateToProps = state => ({
-  jobs: state.jobsReducer.jobs,
-  filter: state.filterReducer,
-  error: state.jobsReducer.error,
-  loading: state.jobsReducer.loading,
-});
-
-const mapDispatchToFilter = dispatch => ({
-  changeFilter: filter => { dispatch(changeFilter(filter)); },
-  fetchJobs: () => dispatch(fetchJobs()),
-});
-
-JobList.propTypes = {
-  jobs: PropTypes.arrayOf(PropTypes.object).isRequired,
-  filter: PropTypes.string.isRequired,
-  fetchJobs: PropTypes.func.isRequired,
-  changeFilter: PropTypes.func.isRequired,
-  error: PropTypes.string,
-  loading: PropTypes.bool.isRequired,
-};
-
-JobList.defaultProps = {
-  error: null,
-};
-
-export default connect(mapStateToProps, mapDispatchToFilter)(JobList);
+export default JobList;
