66c610780a015f56a1c505c40f7591b1d663abff
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-12 23:56:46 +0300

    take in job and removeJob as props. render job and add onClick functionality for delete button

--- src/components/JobTile.js
+++ src/components/JobTile.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import styles from './JobTile.module.css';
-import jobImg from './job.png';
 
-/*eslint-disable*/
-const JobTile = ({ job }) => (
-  <div className={styles.card}>
+
+const JobTile = ({ job, removeJob }) => (
+  <div>
+    <div>{job.company}</div>
+    <div>{job.position}</div>
+    <div>{job.description}</div>
     <div>
-      <img src={job.company_logo ? job.company_logo : jobImg} alt="company-logo" className={styles.company_logo} />
-    </div>
-    <div className={styles.title}>{`${job.title.slice(0, 23)}...`}</div>
-    <div>{job.type}</div>
-    <div>{job.created_at.slice(0, 10)}</div>
-    <div>{job.location}</div>
-    <div className={styles.btn_container}>
       <Link to={`/jobs/${job.id}`}>
-        <button type="button" className={[styles.btn, styles.btn__apply].join(' ')}>Apply</button>
+        <button type="button" className={[styles.btn, styles.btn__apply].join(' ')}>Details</button>
       </Link>
-      <button type="button" className={[styles.btn, styles.btn__save].join(' ')}>Save</button>
+      <button onClick={() => removeJob(job.id)} type="button" className={[styles.btn, styles.btn__save].join(' ')}>Delete</button>
     </div>
   </div>
 );
-/*eslint-disable*/
-JobTile.protoTypes = {
-  job: PropTypes.any,
-};
 
 export default JobTile;
