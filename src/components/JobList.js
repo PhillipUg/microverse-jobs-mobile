0ac78d1bf74a83a5e6a315bc0499f9c2ce115a40
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 13:46:23 +0300

    import authService to get current user and Redirect to handle authorization

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,24 +1,31 @@
 import React, { useContext } from 'react';
-import NewJobForm from './NewJobForm';
+// import NewJobForm from './NewJobForm';
 import JobTile from './JobTile'
 import { JobContext } from '../contexts/JobContext';
+import { Redirect } from 'react-router-dom'
+import AuthService from '../services/authService';
+
 
 
 const JobList = () => {
-  const initialFormState = {
-    company: '',
-    position: '',
-    description: ''
-  }
+  // const initialFormState = {
+  //   company: '',
+  //   position: '',
+  //   description: ''
+  // }
 
-  const { jobs, addJob, removeJob } = useContext(JobContext)
+  const { jobs } = useContext(JobContext)
+
+  const user = AuthService.getCurrentUser();
+
+  if (!user) return <Redirect to="/signin" />
 
   return (
-    <div style={{ color: 'white' }}>
+    <div >
       {jobs.map((job, index) => (
-        <JobTile key={index} job={job} removeJob={removeJob} />
+        <JobTile key={index} job={job} />
       ))}
-      <NewJobForm addJob={addJob} initialFormState={initialFormState} />
+      {/* <NewJobForm addJob={addJob} initialFormState={initialFormState} /> */}
     </div>
   )
 
