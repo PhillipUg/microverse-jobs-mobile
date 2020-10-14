5c5e24946b239e89655d29d1be219e757a6a5ee9
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 13:41:05 +0300

    import Navbar, signin and sinup components into app component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import JobList from './components/JobList';
 import JobDetails from './components/JobDetails';
+import Navbar from './components/Navbar';
+import SignUp from './components/auth/SignUp';
+import SignIn from './components/auth/SignIn';
 import JobContextProvider from './contexts/JobContext';
+import FavoritesContextProvider from './contexts/FavoritesContext';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <JobContextProvider>
-          <Switch>
-            <Route exact path="/" component={JobList} />
-            <Route path="/jobs/:id" component={JobDetails} />
-          </Switch>
-        </JobContextProvider>
+        <Navbar />
+        <Switch>
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <JobContextProvider>
+            <FavoritesContextProvider>
+              <Route exact path="/" component={JobList} />
+              <Route path="/jobs/:id" component={JobDetails} />
+            </FavoritesContextProvider>
+          </JobContextProvider>
+        </Switch>
       </div>
     </BrowserRouter>
   );
