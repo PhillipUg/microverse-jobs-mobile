d2b8312fc34e84eff7755aef0906f123bd7350a3
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 13:46:56 +0300

    import authService to get current user and Redirect to handle authorization

--- src/components/JobDetails.js
+++ src/components/JobDetails.js
@@ -1,11 +1,54 @@
 import React, { useContext } from 'react';
 import { JobContext } from '../contexts/JobContext';
 import styles from './JobDetails.module.css';
+import { Redirect } from 'react-router-dom'
+import AuthService from '../services/authService';
+// import axios from 'axios'
+import FavoriteService from '../services/favoriteService';
+// import authHeader from '../services/authHeader';
+import { FavoritesContext } from '../contexts/FavoritesContext';
 
 const JobDetails = (props) => {
   const { jobs } = useContext(JobContext)
+  const { favorites } = useContext(FavoritesContext)
   const id = parseInt(props.match.params.id)
   const job = jobs.filter(job => job.id === id)
+
+  const user = AuthService.getCurrentUser();
+
+  // const [favorited, setFavorited] = useState(false)
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:3001/api/v1/favorites", { headers: authHeader() })
+  //     .then(res => console.log(res))
+  // }, [])
+
+  const res = favorites.filter(fav => fav.job_id === job[0].id && fav.user_id === user.user.id)
+  console.log(res)
+
+  if (!user) return <Redirect to="/signin" />
+
+  // console.log(user.user.id, job[0].id)
+
+
+  // const handleClick = (e) => {
+
+  //   if (e.target.value === "favorite") {
+  //     // setFavorited(true)
+  //     // FavoriteService.favorite(user.user.id, job[0].id)
+  //   } else {
+  //     // setFavorited(false)
+  //     // FavoriteService.unfavorite(user.user.id, job[0].id)
+  //   }
+  // }
+
+  // favorited ? () : ()
+  let btn;
+  if (!res.length) {
+    btn = <button onClick={() => FavoriteService.updateFav(user.user.id, job[0].id)} value="favorite" style={{ background: "yellow" }}>Add to favs</button>
+  } else {
+    btn = <button onClick={() => FavoriteService.updateFav(user.user.id, job[0].id)} value="favorite" style={{ background: "red" }}>Remove from favs</button>
+  }
 
   return (
 
@@ -23,6 +66,8 @@ const JobDetails = (props) => {
         <span>Description:</span>
         {job[0].description}
       </div>
+      {btn}
+
 
     </div>
   );
