117c0690e848cff802a8b536e3c0e92d6fed2dd9
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-14 14:03:55 +0300

    convert to functionsl component. refactor remove addjob and removejob methods. use react hooks

--- src/contexts/JobContext.js
+++ src/contexts/JobContext.js
@@ -1,49 +1,22 @@
-import React, { Component, createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import authHeader from '../services/authHeader'
+// import axios from 'axios'
 
 export const JobContext = createContext();
 
-export default class JobContextProvider extends Component {
-  state = {
-    jobs: []
-  }
+const JobContextProvider = (props) => {
+  const [state, setState] = useState({ jobs: [] })
 
-  componentDidMount() {
-    fetch('https://microverse-jobs-api.herokuapp.com/api/v1/jobs')
+  useEffect(() => {
+    fetch('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', { headers: authHeader() })
       .then((res) => res.json())
-      .then(data => this.setState({ jobs: data }))
-  }
+      .then(data => setState({ jobs: data }))
+  }, [])
 
-  addJob = job => {
-    fetch('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(job)
-    })
-      .then(() => {
-        this.setState({ jobs: [...this.state.jobs, job] })
-        console.log(job)
-      })
-      .catch(err => console.log(err))
-  }
-
-  removeJob = (id) => {
-    fetch('https://microverse-jobs-api.herokuapp.com/api/v1/jobs/' + id, {
-      method: 'DELETE'
-    })
-      .then(() => this.setState({ jobs: this.state.jobs.filter(job => job.id !== id) }))
-      .catch(err => console.log(err))
-
-  }
-
-  render() {
-    return (
-      <JobContext.Provider value={{ ...this.state, addJob: this.addJob, removeJob: this.removeJob }}>
-        {this.props.children}
-      </JobContext.Provider>
-    )
-  }
+  return (
+    <JobContext.Provider value={{ ...state }}>
+      {props.children}
+    </JobContext.Provider>
+  )
 }
-
+export default JobContextProvider;
