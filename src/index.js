7696a886ac7503d84dc067540e95acfb63ee6bf5
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-15 17:28:21 +0300

    remove redux store from app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
