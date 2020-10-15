086b683892b3b1915d8b1655323511f41e3f5204
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-15 17:31:29 +0300

    remove all reducers since we are not using redux

--- src/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import jobsReducer from './jobsReducer';
-import filterReducer from './filterReducer';
-
-const rootReducer = combineReducers({ jobsReducer, filterReducer });
-
-export default rootReducer;
