c34cf76969601af18e0e2e6f4544baa362f6af4c
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-15 17:27:01 +0300

    remove FavoritesContextProvider

--- src/App.js
+++ src/App.js
@@ -3,29 +3,28 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import JobList from './components/JobList';
 import JobDetails from './components/JobDetails';
 import Navbar from './components/Navbar';
+import Favorites from './components/Favorites.js';
 import SignUp from './components/auth/SignUp';
 import SignIn from './components/auth/SignIn';
 import JobContextProvider from './contexts/JobContext';
-import FavoritesContextProvider from './contexts/FavoritesContext';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+    <div className="App">
+      <BrowserRouter>
         <Navbar />
         <Switch>
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
           <JobContextProvider>
-            <FavoritesContextProvider>
-              <Route exact path="/" component={JobList} />
-              <Route path="/jobs/:id" component={JobDetails} />
-            </FavoritesContextProvider>
+            <Route exact path="/" component={JobList} />
+            <Route exact path="/favorites" component={Favorites} />
+            <Route exact path="/jobs/:id" component={JobDetails} />
           </JobContextProvider>
         </Switch>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
