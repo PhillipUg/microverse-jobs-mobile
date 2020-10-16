5d67132a20c841774882fd8c66c829677f876d12
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-17 00:53:11 +0300

    add additional fields like salary and location

--- src/components/JobTile.js
+++ src/components/JobTile.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from '../assets/styles/JobTile.module.css';
 
 
-const JobTile = ({ job, removeJob }) => (
-  <div>
-    <div>{job.company}</div>
-    <div>{job.position}</div>
-    <div>{job.description}</div>
-    <div>
-      <Link to={`/jobs/${job.id}`}>
-        <button type="button" className={[styles.btn, styles.btn__apply].join(' ')}>Details</button>
-      </Link>
-      {/* <button onClick={() => removeJob(job.id)} type="button" className={[styles.btn, styles.btn__save].join(' ')}>Delete</button> */}
+const JobTile = ({ job, currentIndex }) => (
+  <a className={styles.link} href={`/jobs/${job && job.id}`}>
+    <div className={styles.card}>
+      <div className={styles.logo}>{job && <img src={require(`../assets/images/logo${job.id}.jpg`)} alt="company logo" />}</div>
+      <div>{job && job.company}</div>
+      <div>{job && job.position}</div>
+      <div>{job && job.salary}</div>
+      <div>{job && job.location}</div>
+      <p>Job Description</p>
+      <div>{job && job.description}</div>
+      <div>
+      </div>
     </div>
-  </div>
+  </a>
 );
 
 export default JobTile;
