8415d6934568b91bf7f82944884871e38d541fe9
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-17 00:52:48 +0300

    add prev and next buttons to facilitate navigating thru jobs

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -3,11 +3,12 @@ import JobTile from './JobTile'
 import { Redirect } from 'react-router-dom'
 import AuthService from '../services/authService';
 import authHeader from '../services/authHeader';
-
+import styles from '../assets/styles/JobList.module.css'
 
 const JobList = () => {
 
   const [jobs, setJobs] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     fetch('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', { headers: authHeader() })
@@ -19,11 +20,32 @@ const JobList = () => {
 
   if (!user) return <Redirect to="/signin" />
 
+  const prevSlide = () => {
+    const lastIndex = jobs.length - 1;
+    const shouldResetIndex = currentIndex === 0;
+    const index = shouldResetIndex ? lastIndex : currentIndex - 1;
+
+    setCurrentIndex(index);
+  }
+
+  const nextSlide = () => {
+    const lastIndex = jobs.length - 1;
+    const shouldResetIndex = currentIndex === lastIndex;
+    const index = shouldResetIndex ? 0 : currentIndex + 1;
+
+    setCurrentIndex(index);
+  }
+
   return (
-    <div >
-      {jobs && jobs.map((job, index) => (
-        <JobTile key={index} job={job} />
-      ))}
+    <div>
+      <div className={styles.wrapper}>
+        <i className="fas fa-angle-left" onClick={prevSlide}></i>
+        <div className={styles.carousel}>
+          <JobTile key={currentIndex} job={jobs[currentIndex]} />
+        </div>
+        <i className="fas fa-angle-right" onClick={nextSlide}></i>
+      </div>
+      <div className={styles.counter}>{`${currentIndex + 1}/${jobs.length}`}</div>
     </div>
   )
 
