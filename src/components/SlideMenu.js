e511313878fde322d583d4e193bced115e8c6898
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-17 00:54:07 +0300

    add dashboard and favorites fields to slide menu

--- src/components/SlideMenu.js
+++ src/components/SlideMenu.js
@@ -11,23 +11,40 @@ const SlideMenu = ({ slideClass, handleClick }) => {
     const user = AuthService.getCurrentUser();
     if (user) setCurrentUser(user);
   }, [])
-  // console.log(currentUser && currentUser.user)
 
   return (
     <div className={styles.sliding_menu + " " + slideClass}>
       <button type="button" onClick={handleClick}>
         <i className="fas fa-arrow-left"></i>
       </button>
+      <div>
+        <div className={styles.img}></div>
+        <div>
+          @{currentUser && currentUser.user.username}
+        </div>
+      </div>
       <ul>
-        <li>
-          {currentUser && currentUser.user.username}
+        <Link to={"/"}>
+          <li onClick={handleClick}>
+            Dashboard
         </li>
-
-          <Link to={"/favorites"}>
-        <li>
+        </Link>
+        <Link to={"/user-jobs"}>
+          <li onClick={handleClick}>
             Favorites
         </li>
-            </Link>
+        </Link>
+
+        <li>
+          Notifications
+        </li>
+        <li>
+          Messages
+        </li>
+        <hr />
+        <li>
+          Help
+        </li>
         <li >
           <a href="/signin" onClick={AuthService.logout}>
             LogOut
