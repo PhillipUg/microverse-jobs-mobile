f9d8d974573cd807ab13b8665159133c855ebe66
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-17 17:20:13 +0300

    wrap tile with Link tag replace anchor tag

--- src/components/JobTile.js
+++ src/components/JobTile.js
@@ -4,7 +4,7 @@ import styles from '../assets/styles/JobTile.module.css';
 
 
 const JobTile = ({ job }) => (
-  <Link to={`/jobs/${job && job.id}`}>
+  <Link className={styles.link} to={`/jobs/${job && job.id}`}>
     <div className={styles.card}>
       <div className={styles.logo}>{job && <img src={require(`../assets/images/logo${job.id}.jpg`)} alt="company logo" />}</div>
       <div>{job && job.company}</div>
@@ -13,8 +13,6 @@ const JobTile = ({ job }) => (
       <div>{job && job.location}</div>
       <p>Job Description</p>
       <div>{job && job.description}</div>
-      <div>
-      </div>
     </div>
   </Link>
 );
