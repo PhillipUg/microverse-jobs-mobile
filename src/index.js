d0f22342c5aa1e12824af64993a8adb30ecf59d4
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-22 22:59:38 +0300

    import libraries and setup redux store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
 import App from './App';
+import { applyMiddleware, createStore } from 'redux'
+import rootReducer from './reducers/rootReducer'
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
