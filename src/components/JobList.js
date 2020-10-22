15ecd02a18db1964e7fe9f11f6b988b4a4e7a1cf
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-22 23:01:04 +0300

    stop fetching data in component. use useDispatch and useSelector Hooks

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -5,22 +5,33 @@ import JobTile from './JobTile';
 import AuthService from '../services/authService';
 import authHeader from '../services/authHeader';
 import styles from '../assets/styles/JobList.module.css';
+import { useDispatch, useSelector } from 'react-redux'
+import { getData } from '../actions/jobsActions'
 
 const JobList = () => {
-  const [jobs, setJobs] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const jobs = useSelector((state) => state.jobs)
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    axios.get('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', { headers: authHeader() })
-      .then(res => setJobs(res.data));
-  }, []);
+    dispatch(getData())
+  }, [dispatch])
+  // const [jobs, setJobs] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // useEffect(() => {
+  //   axios.get('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', { headers: authHeader() })
+  //     .then(res => setJobs(res.data));
+  // }, []);
 
   const user = AuthService.getCurrentUser();
 
   if (!user) return <Redirect to="/signin" />;
 
+  // console.log(jobs.jobs && jobs.jobs.length - 1)
+
   const prevSlide = () => {
-    const lastIndex = jobs.length - 1;
+    const lastIndex = jobs.jobs && jobs.jobs.length - 1;
     const shouldResetIndex = currentIndex === 0;
     const index = shouldResetIndex ? lastIndex : currentIndex - 1;
 
@@ -28,23 +39,27 @@ const JobList = () => {
   };
 
   const nextSlide = () => {
-    const lastIndex = jobs.length - 1;
+    const lastIndex = jobs.jobs && jobs.jobs.length - 1;
     const shouldResetIndex = currentIndex === lastIndex;
     const index = shouldResetIndex ? 0 : currentIndex + 1;
 
     setCurrentIndex(index);
   };
 
+  const loading = false;
+
   return (
     <section>
+      {loading && <p>Loading...</p>}
       <div className={styles.wrapper}>
         <i className="fas fa-angle-left" onClick={prevSlide} />{/*eslint-disable-line*/}
         <div className={styles.carousel}>
-          <JobTile job={jobs[currentIndex]} />
+          {jobs.jobs && jobs.jobs.map(job => <JobTile key={job.id} job={job} />)}
         </div>
         <i className="fas fa-angle-right" onClick={nextSlide} />{/*eslint-disable-line*/}
       </div>
-      <div className={styles.counter}>{`${currentIndex + 1}/${jobs.length}`}</div>
+      <div className={styles.counter}>{`${currentIndex + 1}/${jobs.jobs && jobs.jobs.length}`}</div>
+
     </section>
   );
 };
