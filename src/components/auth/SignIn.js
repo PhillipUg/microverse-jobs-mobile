169547cad0bc9447fdc8a4b27298d8a13cfb897e
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-25 11:14:23 +0300

    change AuthService and AuthHeader into authActions

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import AuthService from '../../services/authService';
+import { useDispatch } from 'react-redux';
+import { BeatLoader } from 'react-spinners';
 import styles from '../../assets/styles/auth.module.css';
+import { getCurrentUser, login } from '../../actions/authActions';
 
 function SignIn(props) {
-  const [state, setState] = useState({
+  const dispatch = useDispatch();
+  const [mystate, setMystate] = useState({
     username: '',
     password: '',
   });
 
   const [error, setError] = useState('');
 
+  const [loading, setLoading] = useState(false);
+
   const handleChange = e => {
     const { id, value } = e.target;
-    setState(prevState => ({
+    setMystate(prevState => ({
       ...prevState,
       [id]: value,
     }));
@@ -22,20 +27,24 @@ function SignIn(props) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    AuthService.login(state.username, state.password)
+    setLoading(true);
+    setError('');
+    dispatch(login(mystate.username, mystate.password))
       .then(res => {
         if (res.error) {
+          setLoading(false);
           setError(res.error);
         } else {
           props.history.push('/');
           window.location.reload();
         }
-      });
+      })
+      .then(() => setLoading(false));
   };
-
-  const user = AuthService.getCurrentUser();
-
-  if (user) return <Redirect to="/" />;
+  const user = getCurrentUser();
+  if (user) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -43,11 +52,16 @@ function SignIn(props) {
         <p>Sign in</p>
         <p>Hello there! Sign in and start your job hunt</p>
         <p>{error && `! ${error}`}</p>
+        {loading && (
+        <div className="loading">
+          <BeatLoader loading={loading} />
+        </div>
+        )}
         <input
           type="username"
           id="username"
           placeholder="Enter username"
-          value={state.username}
+          value={mystate.username}
           onChange={handleChange}
         />
 
@@ -55,11 +69,11 @@ function SignIn(props) {
           type="password"
           id="password"
           placeholder="Password"
-          value={state.password}
+          value={mystate.password}
           onChange={handleChange}
         />
         <div>
-          <button type="submit" onClick={handleSubmit}> Sign In </button>
+          <button type="submit"> Sign In </button>
           <Link to="/signup">
             <button type="button"> Sign Up </button>
           </Link>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import AuthService from '../../services/authService';
+import { useDispatch } from 'react-redux';
+import { BeatLoader } from 'react-spinners';
 import styles from '../../assets/styles/auth.module.css';
+import { getCurrentUser, register } from '../../actions/authActions';
 
 function SignUp(props) {
-  const [state, setState] = useState({
+  const dispatch = useDispatch();
+  const [mystate, setMystate] = useState({
     username: '',
     password: '',
   });
 
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleChange = e => {
     const { id, value } = e.target;
-    setState(prevState => ({
+    setMystate(prevState => ({
       ...prevState,
       [id]: value,
     }));
@@ -22,19 +26,22 @@ function SignUp(props) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    AuthService.register(state.username, state.password)
+    setError('');
+    setLoading(true);
+    dispatch(register(mystate.username, mystate.password))
       .then(res => {
         if (res.error) {
+          setLoading(false);
           setError(res.error);
         } else {
           props.history.push('/');
           window.location.reload();
         }
-      });
+      })
+      .then(() => setLoading(false));
   };
 
-  const user = AuthService.getCurrentUser();
-
+  const user = getCurrentUser();
   if (user) return <Redirect to="/" />;
 
   return (
@@ -43,12 +50,17 @@ function SignUp(props) {
         <p>Sign up</p>
         <p>Hello there! Sign up and start your job hunt</p>
         <p>{error && `! ${error}`}</p>
+        {loading && (
+        <div className="loading">
+          <BeatLoader loading={loading} />
+        </div>
+        )}
 
         <input
           type="username"
           id="username"
           placeholder="Enter username"
-          value={state.username}
+          value={mystate.username}
           onChange={handleChange}
         />
 
@@ -56,7 +68,7 @@ function SignUp(props) {
           type="password"
           id="password"
           placeholder="Password"
-          value={state.password}
+          value={mystate.password}
           onChange={handleChange}
         />
         <div>
