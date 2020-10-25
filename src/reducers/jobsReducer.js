d1d21abe5e5ec7a2629d5868a954569169c470ec
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-25 11:27:12 +0300

    add jobs reducer

--- src/reducers/jobsReducer.js
+++ src/reducers/jobsReducer.js
@@ -1,16 +1,32 @@
-const initState = [];
+const initState = {
+  jobs: [],
+  loading: false,
+  error: null,
+};
 
 const jobsReducer = (state = initState, action) => {
-  const { type, payload } = action
+  const { type, payload } = action;
   switch (type) {
     case 'SUCCESS_JOBS':
       return {
         ...state,
-        jobs: payload
-      }
+        loading: false,
+        jobs: payload,
+      };
+    case 'BEGIN_JOBS':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'ERROR_JOBS':
+      return {
+        ...state,
+        loading: false,
+        error: payload.error,
+      };
     default:
       return state;
   }
-}
+};
 
 export default jobsReducer;
