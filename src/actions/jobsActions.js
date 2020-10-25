071c2c66d75fcd94f6baf5729f4776994ab8a103
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-25 11:22:35 +0300

    add getJob action

--- src/actions/jobsActions.js
+++ src/actions/jobsActions.js
@@ -1,20 +1,28 @@
-import Axios from "axios"
-import authHeader from "../services/authHeader"
+import Axios from 'axios';
+import authHeader from './authActions';
 
-export const getData = () => {
-  return async (dispatch) => {
-    try {
-      dispatch({
-        type: "AWAITING_JOBS",
-
-      })
-      const response = await Axios.get('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', { headers: authHeader() })
-      dispatch({
-        type: "SUCCESS_JOBS",
-        payload: response.data
-      })
-    } catch (error) {
-
-    }
+export const getData = () => async dispatch => {
+  try {
+    dispatch({ type: 'BEGIN_JOBS' });
+    const response = await Axios.get('https://microverse-jobs-api.herokuapp.com/api/v1/jobs', { headers: authHeader() });
+    dispatch({
+      type: 'SUCCESS_JOBS',
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({ type: 'ERROR_JOBS', payload: error });
   }
-}
+};
+
+export const getJob = id => async dispatch => {
+  try {
+    dispatch({ type: 'BEGIN_JOB' });
+    const response = await Axios.get(`https://microverse-jobs-api.herokuapp.com/api/v1/jobs/${id}`, { headers: authHeader() });
+    dispatch({
+      type: 'SUCCESS_JOB',
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({ type: 'ERROR_JOB', payload: error });
+  }
+};
