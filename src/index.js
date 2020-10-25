2b0b8c91e1a11745eaec19c31099593c25017970
Phillip Musiime <phillipmusiime@gmail.com>
2020-10-25 11:12:43 +0300

    linter rearrage imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
-import App from './App';
-import { applyMiddleware, createStore } from 'redux'
-import rootReducer from './reducers/rootReducer'
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+import App from './App';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
